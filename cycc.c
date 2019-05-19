7f49e2890ee91fdf085bc51e6f86c1550e5ca2fc
cydry3 <ysaita09@gmail.com>
2019-05-20 07:07:22 +0900

    トークナイザを追加

--- cycc.c
+++ cycc.c
@@ -1,5 +1,83 @@
+#include <ctype.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// トークンの型を表す値
+enum {
+  TK_NUM = 256, // 整数トークン
+  TK_EOF,       // 入力の終わりを表すトークン
+};
+
+// トークンの型
+typedef struct {
+  int ty;      // トークンの型
+  int val;     // tyがTK_NUMの場合、その数値
+  char *input; // トークン文字列（エラーメッセージ用）
+} Token;
+
+// 入力プログラム
+char *user_input;
+
+// トークナイズした結果のトークン列をこの配列に保存
+// 100個以上のトークンは来ないものとする　
+Token tokens[100];
+
+// エラーを報告する関数
+// printfと同じ引数を受け取る
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+// エラー箇所を報告する
+void error_at(char *loc, char *msg) {
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, ""); // pos個空白を出力
+  fprintf(stderr, "^ %s\n", msg);
+  exit(1);
+}
+
+// user_input が指している文字列を
+// トークンに分割してtokensに保存
+void tokenize() {
+  char *p = user_input;
+
+  int i = 0;
+  while (*p) {
+    // すきっぷ空白文字
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      tokens[i].ty = *p;
+      tokens[i].input = p;
+      i++;
+      p++;
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      tokens[i].ty = TK_NUM;
+      tokens[i].input = p;
+      tokens[i].val = strtol(p, &p, 10);
+      i++;
+      continue;
+    }
+
+    error_at(p, "トークナイズできません");
+  }
+
+  tokens[i].ty = TK_EOF;
+  tokens[i].input = p;
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,28 +85,44 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  char *p = argv[1];
+  // トークナイズする
+  user_input = argv[1];
+  tokenize();
 
+  // アセンブリの前半分
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", strtol(p, &p, 10));
 
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
+  // 最初の文字列が数であるので、これをチェックして
+  // 最初のmov命令を出力
+  if (tokens[0].ty != TK_NUM)
+    error_at(tokens[0].input, "数ではありません");
+  printf("  mov rax, %d\n", tokens[0].val);
+
+  // +数、または-数というトークン列を消費しながら、
+  // アセンブリを出力する
+  int i = 1;
+  while (tokens[i].ty != TK_EOF) {
+    if (tokens[i].ty == '+') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+        error_at(tokens[i].input, "数ではありません");
+      printf("  add rax, %ld\n", tokens[i].val);
+      i++;
       continue;
     }
 
-    if (*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+    if (tokens[i].ty == '-') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+        error_at(tokens[i].input, "数ではありません");
+      printf("  sub rax, %ld\n", tokens[i].val);
+      i++;
       continue;
     }
 
-    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
-    return 1;
+    error_at(tokens[i].input, "予期しないトークンです");
   }
 
   printf("  ret\n");
